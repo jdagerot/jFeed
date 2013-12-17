e6c9635b7b5b737ce9ca08b15d52598b5f9a0b7b
swebber <batsshadow@gmail.com>
2013-12-17 10:19:10 -0500

    Merged georss coordinates change

--- src/jatom.js
+++ src/jatom.js
@@ -3,9 +3,9 @@ function JAtom(xml) {
 };
 
 JAtom.prototype = {
-    
+
     _parse: function(xml) {
-    
+
         var channel = jQuery('feed', xml).eq(0);
 
         this.version = '1.0';
@@ -14,21 +14,26 @@ JAtom.prototype = {
         this.description = jQuery(channel).find('subtitle:first').text();
         this.language = jQuery(channel).attr('xml:lang');
         this.updated = jQuery(channel).find('updated:first').text();
-        
+
         this.items = new Array();
-        
+
         var feed = this;
-        
+
         jQuery('entry', xml).each( function() {
-        
+
             var item = new JFeedItem();
-            
+
             item.title = jQuery(this).find('title').eq(0).text();
             item.link = jQuery(this).find('link').eq(0).attr('href');
             item.description = jQuery(this).find('content').eq(0).text();
             item.updated = jQuery(this).find('updated').eq(0).text();
             item.id = jQuery(this).find('id').eq(0).text();
-            
+            var point = jQuery(this).find('[nodeName="georss:point"]').eq(0).text();
+            if (point.length > 0) {
+              point = point.split(" ");
+              item.geometry = { type: "Point", coordinates: [point[1], point[0]] };
+            }
+
             feed.items.push(item);
         });
     }
